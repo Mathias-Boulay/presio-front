a200d144545aaaf94e3b5a311925a0ca90fb6470
Mathias-Boulay <mathiasboulay@free.fr>
2022-12-09 18:21:44 +0100

    Feat: Add mocking pres, for ui building purposes

---
+++ src/assets/ts/mock/mockPres.ts
@@ -0,0 +1,88 @@
+import { LightSourceType, type Presentation } from "../Presentation"
+
+/**
+ * Simple file to have some pres data
+ * Since I didn't build the backend
+ */
+export const mockPres: Presentation = {
+  name: 'mock Pres',
+  devices: [
+    {
+      model: 'samsung-s22',
+      originalPosition: {
+        x: 0,
+        y: 0,
+        z: 0
+      },
+      deltaPosition: {
+        x: 0,
+        y: 0,
+        z: 0
+      },
+
+      originalRotation: {
+        roll: 0,
+        pitch: 0,
+        yaw: 0
+      },
+      deltaRotation: {
+        roll: 0,
+        pitch: 0,
+        yaw: 0
+      }
+    },
+
+    {
+      model: 'samsung-s20',
+      originalPosition: {
+        x: 0,
+        y: 0,
+        z: 0
+      },
+      deltaPosition: {
+        x: 0,
+        y: 0,
+        z: 0
+      },
+
+      originalRotation: {
+        roll: 0,
+        pitch: 0,
+        yaw: 0
+      },
+      deltaRotation: {
+        roll: 0,
+        pitch: 0,
+        yaw: 0
+      }
+    },
+    
+  ],
+
+  lights: [
+    {
+      type: LightSourceType.Ambiant,
+      originalPosition: {
+        x: 0,
+        y: 0,
+        z: 0
+      },
+      deltaPosition: {
+        x: 0,
+        y: 0,
+        z: 0
+      },
+
+      originalRotation: {
+        roll: 0,
+        pitch: 0,
+        yaw: 0
+      },
+      deltaRotation: {
+        roll: 0,
+        pitch: 0,
+        yaw: 0
+      }
+    }
+  ]
+}
