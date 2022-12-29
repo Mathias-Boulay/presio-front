9dd696faf4ec9b8186023e035f8d1d98cf747a12
Mathias-Boulay <mathiasboulay@free.fr>
2022-12-29 16:58:46 +0100

    Style: Enforce agnostic ui + nunito

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
+import "agnostic-vue/dist/common.min.css";
+import "agnostic-vue/dist/index.css";
+
 </script>
 
 <template>
